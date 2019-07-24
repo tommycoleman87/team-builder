a91aa3811038a06bfe80be6d79466f9cc0a4bfcb
tommycoleman87 <tommiilee@gmail.com>
2019-07-24 18:50:43 -0400

    added edit functionality

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,19 +1,50 @@
-import React, { useState } from 'react';
-import './App.css';
-import Form from './components/Form';
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
 
 
 function App() {
   const [team, setTeam] = useState([]);
-  console.log(team)
-  const teamHandler = (member) => {
-    console.log(team);
-    return setTeam([...team, member])
-   
-  }
+  const [memberToEdit, setMember] = useState();
+
+  const teamHandler = member => {
+    return setTeam([...team, member]);
+  };
+
+ 
+
+  const editMember = member => {
+    const index = team.indexOf(memberToEdit);
+    setMember();
+    console.log(index);
+    return setTeam(
+      team.map((teamMember, i) => {
+        if (i === index) {
+          return (teamMember = member);
+        } else {
+          return teamMember;
+        }
+      })
+    );
+  };
+
   return (
     <div className="App">
-    <Form addTeamMember={member => teamHandler(member)}/>
+      <Form
+        addTeamMember={member => teamHandler(member)}
+        memberToEdit={memberToEdit}
+        editMember={member => editMember(member)}
+      />
+      {team.map((member, index) => {
+        return (
+          <div key={index}>
+            <h2>Name: {member.name}</h2>
+            <h3>Role: {member.role}</h3>
+            <h3>E-mail: {member.email}</h3>
+            <button onClick={() => setMember(member)}>Edit</button>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- team-builder/src/components/Form.js
+++ team-builder/src/components/Form.js
@@ -1,47 +1,89 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-const Form = ({addTeamMember}) => {
+const Form = ({memberToEdit, addTeamMember, editMember}) => {
   const [member, setMember] = useState({ name: "", email: "", role: "" });
 
  const changeHandler = (event) => {
     const { name, value } = event.target;
     setMember({...member, [name]: value})
-    console.log(member)
+   // console.log(member)
   }
+
+  useEffect(() => {
+    if(memberToEdit) {
+      setMember(memberToEdit)
+      console.log(memberToEdit)
+    }
+  }, [memberToEdit])
 
   const addToTeam = (e) => {
     const addMember = addTeamMember;
     //console.log(addTeamMember)
     //console.log(member.name)
     addMember(member);
+    setMember({ name: "", email: "", role: "" });
     e.preventDefault()
   }
 
+  const editTeamMember = (e) => {
+    const edit = editMember;
+    edit(member);
+    setMember({ name: "", email: "", role: "" });
+    console.log('edited')
+    e.preventDefault();
+  }
 
 
+if(memberToEdit) {
   return (
-    <form onSubmit={addToTeam}>
+    <form onSubmit={editTeamMember}>
       <label>
         Name
         <div>
-          <input name="name" onChange={(event) => changeHandler(event)}/>
+          <input name="name"  value={member.name} onChange={(event) => changeHandler(event)}/>
         </div>
       </label>
       <label>
         E-Mail
         <div>
-          <input name="email" onChange={(event) => changeHandler(event)}/>
+          <input name="email"  value={member.email} onChange={(event) => changeHandler(event)}/>
         </div>
       </label>
       <label>
         Role
         <div>
-          <input name="role" onChange={(event) => changeHandler(event)}/>
+          <input name="role"  value={member.role} onChange={(event) => changeHandler(event)}/>
         </div>
       </label>
       <button>Submit</button>
     </form>
   );
+} else {
+  return (
+    <form onSubmit={addToTeam}>
+      <label>
+        Name
+        <div>
+          <input name="name"  value={member.name} onChange={(event) => changeHandler(event)}/>
+        </div>
+      </label>
+      <label>
+        E-Mail
+        <div>
+          <input name="email" value={member.email} onChange={(event) => changeHandler(event)}/>
+        </div>
+      </label>
+      <label>
+        Role
+        <div>
+          <input name="role" value={member.role} onChange={(event) => changeHandler(event)}/>
+        </div>
+      </label>
+      <button>Submit</button>
+    </form>
+  );
+  
+}
 };
 
 export default Form;
